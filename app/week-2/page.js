80f0a2c11980036dbd393847e16b458caecd7379
cathysunkc <cathysun2012@yahoo.com>
2024-01-15 14:22:44 -0700

    Commit for Assignment 2
    Commit for Assignment 2

---
+++ app/week-2/page.js
@@ -0,0 +1,17 @@
+/*
+ * Web Development 2 - CPRG306D
+ * Week 2 - Assignment
+ * Name: Cathy Sun
+ * Date: 2024 Jan 15
+ */
+
+import StudentInfo from './student-info.js'
+
+export default function Page() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Shopping List</h1>
+        <StudentInfo />
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,18 @@
+/*
+ * Web Development 2 - CPRG306D
+ * Week 2 - Assignment
+ * Name: Cathy Sun
+ * Date: 2024 Jan 15
+ */
+
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (<div className="flex min-h-screen flex-col items-center">
+                <p>Cathy Sun</p>
+                <p><u><Link href="https://github.com/cathysunkc" target="_new">Link to my GitHub repository</Link></u></p>
+            </div>);
+}
+  
+
+ 
